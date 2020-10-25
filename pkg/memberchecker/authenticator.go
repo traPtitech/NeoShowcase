bcf1e2ecb76b08d74b4e5c0b5307a7563c415ab8
hijiki51 <hibiki0719euph@gmail.com>
2020-10-25 20:49:23 +0900

    use echo

--- pkg/memberchecker/authenticator.go
+++ pkg/memberchecker/authenticator.go
@@ -1,7 +1,6 @@
 package memberchecker
 
 import (
-	"crypto/rsa"
 	"errors"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/v4"
@@ -9,36 +8,22 @@ import (
 	"strings"
 )
 
-const pubkeyPEM = `
------BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAraewUw7V1hiuSgUvkly9
-X+tcIh0e/KKqeFnAo8WR3ez2tA0fGwM+P8sYKHIDQFX7ER0c+ecTiKpo/Zt/a6AO
-gB/zHb8L4TWMr2G4q79S1gNw465/SEaGKR8hRkdnxJ6LXdDEhgrH2ZwIPzE0EVO1
-eFrDms1jS3/QEyZCJ72oYbAErI85qJDF/y/iRgl04XBK6GLIW11gpf8KRRAh4vuh
-g5/YhsWUdcX+uDVthEEEGOikSacKZMFGZNi8X8YVnRyWLf24QTJnTHEv+0EStNrH
-HnxCPX0m79p7tBfFC2ha2OYfOtA+94ZfpZXUi2r6gJZ+dq9FWYyA0DkiYPUq9QMb
-OQIDAQAB
------END PUBLIC KEY-----
-`
-
-var pubkey *rsa.PublicKey
-
 func (s *Service) authenticate(c echo.Context) error {
 	panic("TODO")
 }
 
-func ExtractTrapExtTokenFromCookie(r *http.Request) string {
-	c, err := r.Cookie("traP_ext_token")
+func ExtractTrapExtTokenFromCookie(c echo.Context) string {
+	token, err := c.Cookie("traP_ext_token")
 	if err != nil {
 		return ""
 	}
-	return c.Value
+	return token.Value
 }
 
-func AuthorizeMemberByToken(w http.ResponseWriter, r *http.Request) {
-	tokenString := ExtractTrapExtTokenFromCookie(r)
+func (s *Service) AuthorizeMemberByToken(c echo.Context) {
+	tokenString := ExtractTrapExtTokenFromCookie(c)
 	if len(tokenString) == 0 {
-		Unauthorized(w, r)
+		Unauthorized(c)
 		return
 	}
 
@@ -46,33 +31,32 @@ func AuthorizeMemberByToken(w http.ResponseWriter, r *http.Request) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, errors.New("invalid token")
 		}
-		pubkey, _ = jwt.ParseRSAPublicKeyFromPEM([]byte(pubkeyPEM))
-		return pubkey, nil
+		return s.pubkey, nil
 	})
 
 	if err != nil || !token.Valid {
-		Unauthorized(w, r)
+		Unauthorized(c)
 		return
 	}
 
 	claims := token.Claims.(jwt.MapClaims)
 
 	if nameI, ok := claims["name"]; ok {
-		w.Header().Set("X-Showcase-User", nameI.(string))
+		c.Response().Header().Set("X-Showcase-User", nameI.(string))
 	} else {
-		Unauthorized(w, r)
+		Unauthorized(c)
 	}
 }
 
-func Unauthorized(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query().Get("type")
+func Unauthorized(c echo.Context) {
+	q := c.Request().URL.Query().Get("type")
 	switch strings.ToLower(q) {
 	case "soft":
-		w.Header().Set("X-Showcase-User", "-")
-		w.WriteHeader(200)
+		c.Response().Header().Set("X-Showcase-User", "-")
+		c.Response().WriteHeader(200)
 	case "hard":
-		w.WriteHeader(403)
+		c.Response().WriteHeader(403)
 	default:
-		w.WriteHeader(403)
+		c.Response().WriteHeader(403)
 	}
 }
