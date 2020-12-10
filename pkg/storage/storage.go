be4a250fe0473f8a9f69120e9db825b17cb5f49c
杉山祐紀 <magichiro-4869@i.softbank.jp>
2020-12-10 18:59:03 +0900

    add comment

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -9,7 +9,7 @@ type Storage interface {
 	Save(filename string, src io.Reader) error
 	Open(filename string) (io.ReadCloser, error)
 	Delete(filename string) error
-	Move(sourcePath, destPath string) error
+	Move(sourcePath, destPath string) error // LocalDir to destination
 	FileExists(filename string) bool
 	SaveDirToTar(filename string, dstpath string, db *sql.DB, buildid string, sid string) error
 	SaveLogFile(filename string, dstpath string, buildid string) error
