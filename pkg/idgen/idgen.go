95b80c1540b522cd564045555e42d9a808e97351
takashi <t.k.mk3.110@gmail.com>
2021-02-02 21:03:43 +0900

    fix id generation

--- pkg/idgen/idgen.go
+++ pkg/idgen/idgen.go
@@ -1,14 +1,11 @@
 package idgen
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"io"
+	"github.com/volatiletech/randomize"
+	"math/rand"
 )
 
 // New 22文字以内のランダムな文字列を生成
 func New() string {
-	var b [16]byte
-	_, _ = io.ReadFull(rand.Reader, b[:])
-	return base64.RawURLEncoding.EncodeToString(b[:])
+	return randomize.Str(rand.Int63, 22)
 }
