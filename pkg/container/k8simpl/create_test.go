62c5b19bb4172c0ec927b400f9d879633daeb566
takashi <t.k.mk3.110@gmail.com>
2021-02-14 15:32:13 +0900

    add tests

--- pkg/container/k8simpl/create_test.go
+++ pkg/container/k8simpl/create_test.go
@@ -13,6 +13,7 @@ func TestManager_Create(t *testing.T) {
 	m, c, _ := prepareManager(t)
 
 	t.Run("Podを正常に起動", func(t *testing.T) {
+		t.Parallel()
 		image := "tianon/sleeping-beauty"
 		appID := "pjpjpjoijion"
 		envID := "fewwfadsface"
@@ -25,6 +26,29 @@ func TestManager_Create(t *testing.T) {
 		if assert.NoError(t, err) {
 			waitPodRunning(t, c, deploymentName(appID, envID))
 			require.NoError(t, c.CoreV1().Pods(appNamespace).Delete(context.Background(), deploymentName(appID, envID), metav1.DeleteOptions{}))
+		}
+	})
+
+	t.Run("Podを正常に起動 (HTTP)", func(t *testing.T) {
+		t.Parallel()
+		image := "chussenot/tiny-server"
+		appID := "pijojopjnnna"
+		envID := "2io3isaoioij"
+
+		_, err := m.Create(context.Background(), container.CreateArgs{
+			ImageName:     image,
+			ApplicationID: appID,
+			EnvironmentID: envID,
+			HTTPProxy: &container.HTTPProxy{
+				Domain: "test.localhost",
+				Port:   80,
+			},
+		})
+		if assert.NoError(t, err) {
+			waitPodRunning(t, c, deploymentName(appID, envID))
+			require.NoError(t, c.CoreV1().Pods(appNamespace).Delete(context.Background(), deploymentName(appID, envID), metav1.DeleteOptions{}))
+			require.NoError(t, c.CoreV1().Services(appNamespace).Delete(context.Background(), deploymentName(appID, envID), metav1.DeleteOptions{}))
+			require.NoError(t, c.NetworkingV1beta1().Ingresses(appNamespace).Delete(context.Background(), deploymentName(appID, envID), metav1.DeleteOptions{}))
 		}
 	})
 }
--- pkg/container/k8simpl/destroy_test.go
+++ pkg/container/k8simpl/destroy_test.go
@@ -12,6 +12,7 @@ func TestManager_Destroy(t *testing.T) {
 	m, c, _ := prepareManager(t)
 
 	t.Run("Podを正常に削除", func(t *testing.T) {
+		t.Parallel()
 		image := "tianon/sleeping-beauty"
 		appID := "ap8ajievpjap"
 		envID := "24j0jadlskfj"
@@ -20,6 +21,31 @@ func TestManager_Destroy(t *testing.T) {
 			ImageName:     image,
 			ApplicationID: appID,
 			EnvironmentID: envID,
+		})
+		require.NoError(t, err)
+		waitPodRunning(t, c, deploymentName(appID, envID))
+
+		_, err = m.Destroy(context.Background(), container.DestroyArgs{
+			ApplicationID: appID,
+			EnvironmentID: envID,
+		})
+		assert.NoError(t, err)
+	})
+
+	t.Run("Podを正常に削除 (HTTP)", func(t *testing.T) {
+		t.Parallel()
+		image := "chussenot/tiny-server"
+		appID := "pjpoi2efeioji"
+		envID := "908uyoinkasdf"
+
+		_, err := m.Create(context.Background(), container.CreateArgs{
+			ImageName:     image,
+			ApplicationID: appID,
+			EnvironmentID: envID,
+			HTTPProxy: &container.HTTPProxy{
+				Domain: "test.localhost",
+				Port:   80,
+			},
 		})
 		require.NoError(t, err)
 		waitPodRunning(t, c, deploymentName(appID, envID))
