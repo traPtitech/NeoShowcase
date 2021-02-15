500387cb432284f70cbf703ce2054b5597acfbd6
takashi <t.k.mk3.110@gmail.com>
2021-02-15 14:15:14 +0900

    update Ingress Resource Version

--- pkg/container/k8simpl/util.go
+++ pkg/container/k8simpl/util.go
@@ -1,8 +1,12 @@
 package k8simpl
 
-import "fmt"
+import (
+	"fmt"
+	networkingv1 "k8s.io/api/networking/v1"
+)
 
-func int32Ptr(i int32) *int32 { return &i }
+func int32Ptr(i int32) *int32                                           { return &i }
+func pathTypePtr(pathType networkingv1.PathType) *networkingv1.PathType { return &pathType }
 
 func deploymentName(appID, envID string) string {
 	return fmt.Sprintf("nsapp-%s-%s", appID, envID)
