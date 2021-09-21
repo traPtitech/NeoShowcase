83f60457ba94f38058c65f015b95082fd8090c48
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-09-21 23:16:39 +0900

    :bug:fix exec order

--- migrations/20210826221725-rename-environments-table.sql
+++ migrations/20210826221725-rename-environments-table.sql
@@ -1,16 +1,20 @@
 -- +migrate Up
+ALTER TABLE `websites`
+    DROP FOREIGN KEY `fk_websites_environment_id`,
+    DROP INDEX `uk_environment_id`;
+
+ALTER TABLE `build_logs`
+    DROP FOREIGN KEY `fk_build_logs_environment_id`;
+
 ALTER TABLE `environments`
     MODIFY COLUMN `id` varchar(22) COLLATE 'utf8mb4_general_ci' NOT NULL COMMENT 'ブランチID' FIRST,
     RENAME TO `branches`;
 
 ALTER TABLE `build_logs`
-    DROP FOREIGN KEY `fk_build_logs_environment_id`,
     CHANGE `environment_id` `branch_id` varchar(22) COLLATE 'utf8mb4_general_ci' NOT NULL COMMENT 'ブランチID' AFTER `finished_at`,
     ADD CONSTRAINT `fk_build_logs_branch_id` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 ALTER TABLE `websites`
-    DROP FOREIGN KEY `fk_websites_environment_id`,
-    DROP INDEX `uk_environment_id`,
     CHANGE `environment_id` `branch_id` varchar(22) COLLATE 'utf8mb4_general_ci' NOT NULL COMMENT 'ブランチID' AFTER `updated_at`,
     ADD UNIQUE `uk_branch_id` (`branch_id`),
     ADD CONSTRAINT `fk_websites_branch_id` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT;
