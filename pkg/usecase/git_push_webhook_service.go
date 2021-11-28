8666daa7dfa9ca9d13612db5c8cd5ab12307f047
hijiki51 <hibiki0719euph@gmail.com>
2021-11-28 12:47:10 +0900

    ✨  add CheckRepostoryExsits to GitPushWebhookService

--- pkg/usecase/git_push_webhook_service.go
+++ pkg/usecase/git_push_webhook_service.go
@@ -13,18 +13,29 @@ import (
 
 type GitPushWebhookService interface {
 	VerifySignature(ctx context.Context, repoURL string, signature string, body []byte) (valid bool, err error)
+	CheckRepositoryExists(ctx context.Context, args CheckRepositoryExistsArgs) (exists bool, err error)
 }
 
 type gitPushWebhookService struct {
-	repository.WebhookSecretRepository
+	gitRepo    repository.GitrepositoryRepository
+	secretRepo repository.WebhookSecretRepository
 }
 
-func NewGitPushWebhookService(repo repository.WebhookSecretRepository) GitPushWebhookService {
-	return &gitPushWebhookService{WebhookSecretRepository: repo}
+type CheckRepositoryExistsArgs struct {
+	ProviderID string
+	Owner      string
+	RepoName   string
+}
+
+func NewGitPushWebhookService(secretRepo repository.WebhookSecretRepository, gitRepo repository.GitrepositoryRepository) GitPushWebhookService {
+	return &gitPushWebhookService{
+		secretRepo: secretRepo,
+		gitRepo:    gitRepo,
+	}
 }
 
 func (s *gitPushWebhookService) VerifySignature(ctx context.Context, repoURL string, signature string, body []byte) (bool, error) {
-	secrets, err := s.GetWebhookSecretKeys(ctx, repoURL)
+	secrets, err := s.secretRepo.GetWebhookSecretKeys(ctx, repoURL)
 	if err != nil {
 		return false, fmt.Errorf("failed to GetWebhookSecretKeys: %w", err)
 	}
@@ -35,4 +46,23 @@ func (s *gitPushWebhookService) VerifySignature(ctx context.Context, repoURL str
 		valid = valid || domain.VerifySignature(sha256.New, body, []byte(secret), signature)
 	}
 	return valid, nil
+}
+
+func (s *gitPushWebhookService) CheckRepositoryExists(ctx context.Context, args CheckRepositoryExistsArgs) (bool, error) {
+	r := repository.GetRepositoryArgs{
+		ProviderID: args.ProviderID,
+		Owner:      args.Owner,
+		Name:       args.RepoName,
+	}
+	_, err := s.gitRepo.GetRepository(ctx, r)
+
+	if err != nil {
+		if err == repository.ErrNotFound {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to GetRepository: %w", err)
+	}
+
+	return true, nil
+
 }
