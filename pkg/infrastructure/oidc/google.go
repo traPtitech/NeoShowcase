6103379f97c37542df8c4378be7e08d095879a85
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-23 22:54:04 +0900

    github -> google

---
+++ pkg/infrastructure/oidc/google.go
@@ -0,0 +1,27 @@
+package oidc
+
+import (
+	"context"
+	"github.com/coreos/go-oidc/v3/oidc"
+	"golang.org/x/oauth2"
+)
+
+func NewGoogleOIDCProvider(ctx context.Context, clientID, clientSecret string) (*oauth2.Config, *oidc.IDTokenVerifier, error) {
+	provider, err := oidc.NewProvider(ctx, "https://accounts.google.com")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	config := &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint: provider.Endpoint(),
+		Scopes: []string{oidc.ScopeOpenID,"profile"},
+	}
+
+	verifier := provider.Verifier(&oidc.Config{
+		ClientID: clientID,
+	})
+
+	return config, verifier, nil
+}
