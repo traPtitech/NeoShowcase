9aaf06dcfb97a622f9d1844d10221b321baf0c78
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-24 00:40:11 +0900

    LoginHandlerのコンストラクタとServeHTTPの生成

--- pkg/domain/oidc.go
+++ pkg/domain/oidc.go
@@ -1,13 +1,44 @@
 package domain
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"time"
+)
 
-func NewLoginHandler() http.Handler {
+type LoginHandler struct {
+	clientID     string
+	clientSecret string
+}
+
+func NewLogin() *LoginHandler {
+	clientID := os.Getenv("clientID")
+	clientSecret := os.Getenv("clientSecret")
+	return &LoginHandler{clientID: clientID, clientSecret: clientSecret}
+}
+
+func (l *LoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+func NewLoginHandler(w http.ResponseWriter, r *http.Request) http.Handler {
 	// TODO: Provider等の必要なものを受け取って、各プロバイダー向けのログイン用ハンドラを返す関数
 	return http.NotFoundHandler()
 }
 
-func NewCallbackHandler() http.Handler {
+func NewCallbackHandler(w http.ResponseWriter, r *http.Request) http.Handler {
 	// TODO: Provider等の必要なものを受け取って、各プロバイダー向けのコールバック用ハンドラを返す関数
 	return http.NotFoundHandler()
+}
+
+func setCallbackCookie(w http.ResponseWriter, r *http.Request, name, value string) {
+	c := &http.Cookie{
+		Name:     name,
+		Value:    value,
+		MaxAge:   int(time.Hour.Seconds()),
+		Secure:   r.TLS != nil,
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, c)
 }
