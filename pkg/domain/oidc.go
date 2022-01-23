bd3a4c29db2b33b19623b51c0cb4c78c08be2b01
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-24 00:42:25 +0900

    randomStringの実装

--- pkg/domain/oidc.go
+++ pkg/domain/oidc.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+	"github.com/traPtitech/neoshowcase/pkg/infrastructure/oidc"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
@@ -23,6 +25,13 @@ func (l *LoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func NewLoginHandler(w http.ResponseWriter, r *http.Request) http.Handler {
 	// TODO: Provider等の必要なものを受け取って、各プロバイダー向けのログイン用ハンドラを返す関数
+
+	l := NewLogin()
+	config, verifier, err := oidc.NewGoogleOIDCProvider(r.Context(), l.clientID, l.clientSecret)
+	if err != nil {
+		return nil
+	}
+
 	return http.NotFoundHandler()
 }
 
@@ -41,4 +50,14 @@ func setCallbackCookie(w http.ResponseWriter, r *http.Request, name, value strin
 	}
 
 	http.SetCookie(w, c)
+}
+
+func randomString(n int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
 }
