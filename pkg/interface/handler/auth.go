729748294cdb48a45da83947f591d57852fb3e89
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-23 22:54:25 +0900

    基盤の作成

--- pkg/interface/handler/auth.go
+++ pkg/interface/handler/auth.go
@@ -1,3 +1,16 @@
 package handler
 
+import "net/http"
+
 // TODO: signup & login
+type AuthHandler Handler
+
+type authHandler struct {
+	
+}
+
+func (a *authHandler) HandleRequest(c Context) error {
+
+	return c.NoContent(http.StatusOK)
+}
+
