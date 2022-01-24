c8fcbeb3b7fe4eb04d4f6b2e4fc9cb8ea227606c
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-24 15:10:42 +0900

    domainの呼び出し

--- pkg/interface/handler/auth.go
+++ pkg/interface/handler/auth.go
@@ -1,16 +1,20 @@
 package handler
 
-import "net/http"
+import (
+	"github.com/traPtitech/neoshowcase/pkg/domain"
+	"net/http"
+)
 
 // TODO: signup & login
 type AuthHandler Handler
 
 type authHandler struct {
-	
 }
 
 func (a *authHandler) HandleRequest(c Context) error {
+	lh := domain.NewLoginHandler()
+
+	ch := domain.NewCallbackHandler()
 
 	return c.NoContent(http.StatusOK)
 }
-
