aabedc435f706765a8138e16aa1b00b4fe9f0e26
motoki317 <motoki317@gmail.com>
2023-03-28 09:23:32 +0900

    fix statefulset pod deletion

--- pkg/infrastructure/backend/k8simpl/destroy_container.go
+++ pkg/infrastructure/backend/k8simpl/destroy_container.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 
 	"github.com/traPtitech/neoshowcase/pkg/domain"
 )
@@ -16,9 +17,19 @@ func (b *k8sBackend) DestroyContainer(ctx context.Context, app *domain.Applicati
 		return fmt.Errorf("failed to destroy runtime ingress resources: %w", err)
 	}
 
-	err = b.client.AppsV1().StatefulSets(appNamespace).Delete(ctx, deploymentName(app.ID), metav1.DeleteOptions{})
-	if err != nil && !errors.IsNotFound(err) {
-		return fmt.Errorf("failed to delete pod: %w", err)
+	statefulSet := &v1.StatefulSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "StatefulSet",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName(app.ID),
+			Namespace: appNamespace,
+		},
+		Spec: v1.StatefulSetSpec{
+			Replicas: pointer.Int32(0),
+		},
 	}
-	return nil
+
+	return strategicPatch[*v1.StatefulSet](ctx, statefulSet.Name, statefulSet, b.client.AppsV1().StatefulSets(appNamespace))
 }
