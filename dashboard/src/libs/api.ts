26bf9eedc7b90276564340d22dd69c7b312465cb
motoki317 <motoki317@gmail.com>
2023-04-02 12:35:17 +0900

    use get me from dashboard

--- dashboard/src/libs/api.ts
+++ dashboard/src/libs/api.ts
@@ -1,7 +1,10 @@
 import { createConnectTransport, createPromiseClient } from '@bufbuild/connect-web'
 import { ApplicationService } from '/@/api/neoshowcase/protobuf/apiserver_connect'
+import { createResource } from 'solid-js'
 
 const transport = createConnectTransport({
   baseUrl: '',
 })
 export const client = createPromiseClient(ApplicationService, transport)
+
+export const [user] = createResource(() => client.getMe({}))
