5bdf1b505d89032b3a168945f202a970a3a0ccaa
motoki317 <motoki317@gmail.com>
2023-05-07 13:45:36 +0900

    fix: close channel on connection error

--- pkg/infrastructure/log/loki/streamer.go
+++ pkg/infrastructure/log/loki/streamer.go
@@ -89,12 +89,14 @@ func (l *lokiStreamer) Stream(ctx context.Context, appID string, after time.Time
 
 	ch := make(chan *domain.ContainerLog, 100)
 
+	ctx, cancel := context.WithCancel(ctx)
 	go func() {
 		<-ctx.Done()
 		_ = conn.Close()
 		defer close(ch)
 	}()
 	go func() {
+		defer cancel()
 		defer log.Infof("closing loki websocket stream")
 		log.Infof("new loki websocket stream")
 
