2a57b3764cf4a336f2378a9236ef75459774f6d7
motoki317 <motoki317@gmail.com>
2023-06-19 18:38:50 +0900

    persist top apps page state resolves #633

---
+++ dashboard/src/libs/localStore.ts
@@ -0,0 +1,18 @@
+import { createEffect, createSignal } from 'solid-js'
+import { createStore } from 'solid-js/store'
+
+// https://www.solidjs.com/examples/todos
+
+export const createLocalSignal = <T,>(name: string, init: T): ReturnType<typeof createSignal<T>> => {
+  const localState = localStorage.getItem(name)
+  const [state, setState] = createSignal<T>(localState ? JSON.parse(localState) : init)
+  createEffect(() => localStorage.setItem(name, JSON.stringify(state())))
+  return [state, setState]
+}
+
+export const createLocalStore = <T extends Object>(name: string, init: T): ReturnType<typeof createStore<T>> => {
+  const localState = localStorage.getItem(name)
+  const [state, setState] = createStore<T>(localState ? JSON.parse(localState) : init)
+  createEffect(() => localStorage.setItem(name, JSON.stringify(state)))
+  return [state, setState]
+}
