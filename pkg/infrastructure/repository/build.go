traPtitech/NeoShowcase

commit 3faa717acb7ddf6f6e628e16c7cd713622f5fea6

rebuild on config change resolves #645

pkg/infrastructure/repository/build.go
@@ -27,6 +27,9 @@ func NewBuildRepository(db *sql.DB) domain.BuildRepository {
 
 func (r *buildRepository) buildMods(cond domain.GetBuildCondition) []qm.QueryMod {
 	var mods []qm.QueryMod
+	if cond.ID.Valid {
+		mods = append(mods, models.BuildWhere.ID.EQ(cond.ID.V))
+	}
 	if cond.ApplicationID.Valid {
 		mods = append(mods, models.BuildWhere.ApplicationID.EQ(cond.ApplicationID.V))
 	}
@@ -35,6 +38,9 @@ func (r *buildRepository) buildMods(cond domain.GetBuildCondition) []qm.QueryMod
 	}
 	if cond.CommitIn.Valid {
 		mods = append(mods, models.BuildWhere.Commit.IN(cond.CommitIn.V))
+	}
+	if cond.ConfigHash.Valid {
+		mods = append(mods, models.BuildWhere.ConfigHash.EQ(cond.ConfigHash.V))
 	}
 	if cond.Status.Valid {
 		mods = append(mods, models.BuildWhere.Status.EQ(repoconvert.BuildStatusMapper.FromMust(cond.Status.V)))
@@ -78,60 +84,28 @@ func (r *buildRepository) CreateBuild(ctx context.Context, build *domain.Build) 
 	return nil
 }
 
-func (r *buildRepository) UpdateBuild(ctx context.Context, id string, args domain.UpdateBuildArgs) error {
-	mods := []qm.QueryMod{
-		models.BuildWhere.ID.EQ(id),
-		qm.For("UPDATE"),
-	}
-
-	if args.FromStatus.Valid {
-		mods = append(mods, models.BuildWhere.Status.EQ(repoconvert.BuildStatusMapper.FromMust(args.FromStatus.V)))
-	}
-
-	tx, err := r.db.BeginTx(ctx, nil)
-	if err != nil {
-		return errors.Wrap(err, "failed to start transaction")
-	}
-	defer tx.Rollback()
-
-	build, err := models.Builds(mods...).One(ctx, tx)
-	if err != nil {
-		if isNoRowsErr(err) {
-			return ErrNotFound
-		}
-		return errors.Wrap(err, "failed to get build")
-	}
-
-	var cols []string
+func (r *buildRepository) UpdateBuild(ctx context.Context, cond domain.GetBuildCondition, args domain.UpdateBuildArgs) error {
+	cols := make(models.M)
 	if args.Status.Valid {
-		build.Status = repoconvert.BuildStatusMapper.FromMust(args.Status.V)
-		cols = append(cols, models.BuildColumns.Status)
+		cols[models.BuildColumns.Status] = repoconvert.BuildStatusMapper.FromMust(args.Status.V)
 	}
 	if args.StartedAt.Valid {
-		build.StartedAt = optional.IntoTime(args.StartedAt)
-		cols = append(cols, models.BuildColumns.StartedAt)
+		cols[models.BuildColumns.StartedAt] = optional.IntoTime(args.StartedAt)
 	}
 	if args.UpdatedAt.Valid {
-		build.UpdatedAt = optional.IntoTime(args.UpdatedAt)
-		cols = append(cols, models.BuildColumns.UpdatedAt)
+		cols[models.BuildColumns.UpdatedAt] = optional.IntoTime(args.UpdatedAt)
 	}
 	if args.FinishedAt.Valid {
-		build.FinishedAt = optional.IntoTime(args.FinishedAt)
-		cols = append(cols, models.BuildColumns.FinishedAt)
+		cols[models.BuildColumns.FinishedAt] = optional.IntoTime(args.FinishedAt)
+	}
+	if len(cols) == 0 {
+		return nil
 	}
 
-	if len(cols) > 0 {
-		_, err = build.Update(ctx, tx, boil.Whitelist(cols...))
-		if err != nil {
-			return errors.Wrap(err, "failed to update build")
-		}
-	}
-
-	err = tx.Commit()
+	_, err := models.Builds(r.buildMods(cond)...).UpdateAll(ctx, r.db, cols)
 	if err != nil {
-		return errors.Wrap(err, "failed to commit")
+		return errors.Wrap(err, "failed to update build")
 	}
-
 	return nil
 }
 

pkg/infrastructure/repository/repoconvert/build.go
@@ -21,6 +21,7 @@ func FromDomainBuild(build *domain.Build) *models.Build {
 	return &models.Build{
 		ID:            build.ID,
 		Commit:        build.Commit,
+		ConfigHash:    build.ConfigHash,
 		Status:        BuildStatusMapper.FromMust(build.Status),
 		QueuedAt:      build.QueuedAt,
 		StartedAt:     optional.IntoTime(build.StartedAt),
@@ -35,6 +36,7 @@ func ToDomainBuild(build *models.Build) *domain.Build {
 	ret := &domain.Build{
 		ID:            build.ID,
 		Commit:        build.Commit,
+		ConfigHash:    build.ConfigHash,
 		Status:        BuildStatusMapper.IntoMust(build.Status),
 		ApplicationID: build.ApplicationID,
 		QueuedAt:      build.QueuedAt,

pkg/usecase/builder/build.go
@@ -26,11 +26,13 @@ func (s *builderService) tryStartBuild(buildID string) error {
 	}
 
 	now := time.Now()
-	err := s.buildRepo.UpdateBuild(context.Background(), buildID, domain.UpdateBuildArgs{
-		FromStatus: optional.From(domain.BuildStatusQueued),
-		Status:     optional.From(domain.BuildStatusBuilding),
-		StartedAt:  optional.From(now),
-		UpdatedAt:  optional.From(now),
+	err := s.buildRepo.UpdateBuild(context.Background(), domain.GetBuildCondition{
+		ID:     optional.From(buildID),
+		Status: optional.From(domain.BuildStatusQueued),
+	}, domain.UpdateBuildArgs{
+		Status:    optional.From(domain.BuildStatusBuilding),
+		StartedAt: optional.From(now),
+		UpdatedAt: optional.From(now),
 	})
 	if err == repository.ErrNotFound {
 		return nil // other builder has acquired the build lock - skip
@@ -214,7 +216,9 @@ func (s *builderService) updateStatusLoop(ctx context.Context, buildID string) {
 	for {
 		select {
 		case <-ticker.C:
-			err := s.buildRepo.UpdateBuild(ctx, buildID, domain.UpdateBuildArgs{UpdatedAt: optional.From(time.Now())})
+			err := s.buildRepo.UpdateBuild(ctx,
+				domain.GetBuildCondition{ID: optional.From(buildID)},
+				domain.UpdateBuildArgs{UpdatedAt: optional.From(time.Now())})
 			if err != nil {
 				log.Errorf("failed to update build time: %+v", err)
 			}
@@ -231,12 +235,13 @@ func (s *builderService) finalize(ctx context.Context, st *state, status domain.
 	}
 
 	now := time.Now()
+	updateCond := domain.GetBuildCondition{ID: optional.From(st.build.ID)}
 	updateArgs := domain.UpdateBuildArgs{
 		Status:     optional.From(status),
 		UpdatedAt:  optional.From(now),
 		FinishedAt: optional.From(now),
 	}
-	if err := s.buildRepo.UpdateBuild(ctx, st.build.ID, updateArgs); err != nil {
+	if err := s.buildRepo.UpdateBuild(ctx, updateCond, updateArgs); err != nil {
 		log.Errorf("failed to update build: %+v", err)
 	}
 }

migrations/schema.sql
@@ -148,6 +148,7 @@ CREATE TABLE `builds`
 (
     `id`             CHAR(22)    NOT NULL COMMENT 'ビルドID',
     `commit`         CHAR(40)    NOT NULL COMMENT 'コミットハッシュ',
+    `config_hash`    CHAR(16)    NOT NULL COMMENT 'ビルド設定のハッシュ',
     `status`         ENUM (
         'building',
         'succeeded',

pkg/domain/app.go
@@ -1,11 +1,14 @@
 package domain
 
 import (
+	"encoding/json"
 	"strings"
 	"time"
 
 	"github.com/friendsofgo/errors"
 	"github.com/samber/lo"
+
+	"github.com/traPtitech/neoshowcase/pkg/util/hash"
 )
 
 type ApplicationConfig struct {
@@ -20,6 +23,11 @@ func (c *ApplicationConfig) Validate(deployType DeployType) error {
 		return errors.Wrap(err, "invalid build_config")
 	}
 	return nil
+}
+
+func (c *ApplicationConfig) Hash() string {
+	b := lo.Must(json.Marshal(c))
+	return hash.XXH3Hex(b)
 }
 
 type DeployType int

pkg/domain/app_build.go
@@ -34,6 +34,7 @@ func (t BuildStatus) IsFinished() bool {
 type Build struct {
 	ID            string
 	Commit        string
+	ConfigHash    string
 	Status        BuildStatus
 	ApplicationID string
 	QueuedAt      time.Time
@@ -44,12 +45,13 @@ type Build struct {
 	Artifacts     []*Artifact
 }
 
-func NewBuild(applicationID string, commit string) *Build {
+func NewBuild(app *Application) *Build {
 	return &Build{
 		ID:            NewID(),
-		Commit:        commit,
+		Commit:        app.WantCommit,
+		ConfigHash:    app.Config.Hash(),
 		Status:        BuildStatusQueued,
-		ApplicationID: applicationID,
+		ApplicationID: app.ID,
 		QueuedAt:      time.Now(),
 	}
 }

pkg/domain/app_test.go
@@ -204,6 +204,7 @@ func TestApplicationConfig_Validate(t *testing.T) {
 			wantErr: true,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.config.Validate(tt.deployType)
@@ -211,6 +212,16 @@ func TestApplicationConfig_Validate(t *testing.T) {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
+			}
+		})
+	}
+	for _, tt := range tests {
+		t.Run(tt.name+" (hash)", func(t *testing.T) {
+			xxh3 := tt.config.Hash()
+			t.Logf("hash: %v", xxh3)
+			assert.Len(t, xxh3, 16)
+			for i := 0; i < 5; i++ {
+				assert.Equal(t, xxh3, tt.config.Hash())
 			}
 		})
 	}

pkg/domain/repository.go
@@ -99,15 +99,16 @@ type ArtifactRepository interface {
 }
 
 type GetBuildCondition struct {
+	ID            optional.Of[string]
 	ApplicationID optional.Of[string]
 	Commit        optional.Of[string]
 	CommitIn      optional.Of[[]string]
+	ConfigHash    optional.Of[string]
 	Status        optional.Of[BuildStatus]
 	Retriable     optional.Of[bool]
 }
 
 type UpdateBuildArgs struct {
-	FromStatus optional.Of[BuildStatus]
 	Status     optional.Of[BuildStatus]
 	StartedAt  optional.Of[time.Time]
 	UpdatedAt  optional.Of[time.Time]
@@ -118,7 +119,7 @@ type BuildRepository interface {
 	GetBuilds(ctx context.Context, cond GetBuildCondition) ([]*Build, error)
 	GetBuild(ctx context.Context, buildID string) (*Build, error)
 	CreateBuild(ctx context.Context, build *Build) error
-	UpdateBuild(ctx context.Context, id string, args UpdateBuildArgs) error
+	UpdateBuild(ctx context.Context, cond GetBuildCondition, args UpdateBuildArgs) error
 	MarkCommitAsRetriable(ctx context.Context, applicationID string, commit string) error
 	DeleteBuilds(ctx context.Context, cond GetBuildCondition) error
 }

commit 854cc73c42501e7b452538a8731e4060d3343b48

fix builder start race bug introduced in 3faa717a

pkg/infrastructure/repository/build.go
@@ -118,7 +118,9 @@ func (r *buildRepository) UpdateBuild(ctx context.Context, cond domain.GetBuildC
 		return nil
 	}
 
-	_, err := models.Builds(r.buildMods(cond)...).UpdateAll(ctx, r.db, cols)
+	mods := []qm.QueryMod{qm.For("UPDATE")}
+	mods = append(mods, r.buildMods(cond)...)
+	_, err := models.Builds(mods...).UpdateAll(ctx, r.db, cols)
 	if err != nil {
 		return errors.Wrap(err, "failed to update build")
 	}