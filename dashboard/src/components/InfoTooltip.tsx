79df255eeee102eae5cc8986a62e92a2a5103e64
motoki317 <motoki317@gmail.com>
2023-07-16 22:13:35 +0900

    impl multiline tooltip

--- dashboard/src/components/InfoTooltip.tsx
+++ dashboard/src/components/InfoTooltip.tsx
@@ -1,8 +1,10 @@
-import { Component } from 'solid-js'
+import { Component, createMemo, For } from 'solid-js'
 import { styled } from '@macaron-css/solid'
 import { AiOutlineInfoCircle } from 'solid-icons/ai'
 import { tippy as tippyDir, TippyOptions } from 'solid-tippy'
 import 'tippy.js/dist/tippy.css'
+import 'tippy.js/animations/shift-away-subtle.css'
+import { Content } from 'tippy.js'
 
 declare module 'solid-js' {
   namespace JSX {
@@ -28,14 +30,31 @@ const Container = styled('div', {
   },
 })
 
+const TooltipContainer = styled('div', {
+  base: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+})
+
 export interface InfoTooltipProps {
-  tooltip: string
+  tooltip: string | string[]
 }
 
 export const InfoTooltip: Component<InfoTooltipProps> = (props) => {
+  const content = createMemo((): Content => {
+    if (typeof props.tooltip === 'string') return props.tooltip
+    return (
+      <TooltipContainer>
+        <For each={props.tooltip}>{(line) => <span>{line}</span>}</For>
+      </TooltipContainer>
+    ) as Element
+  })
+
   return (
     <Container>
-      <div use:tippy={{ props: { content: props.tooltip }, hidden: true }}>
+      <div use:tippy={{ props: { content: content(), animation: 'shift-away-subtle', allowHTML: true }, hidden: true }}>
         <AiOutlineInfoCircle />
       </div>
     </Container>
