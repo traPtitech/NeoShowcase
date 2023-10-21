f597a245a4c839b03d1ac32015680fd43d4ac117
pirosiki197 <lingmukangtai825@gmail.com>
2023-10-22 03:12:29 +0900

    ValidateDomainの中で検証

--- pkg/domain/app_website.go
+++ pkg/domain/app_website.go
@@ -13,6 +13,10 @@ import (
 )
 
 func ValidateDomain(domain string) error {
+	// ドメインが大文字を含むときはエラー
+	if domain != strings.ToLower(domain) {
+		return errors.Errorf("domain %v must be lower case", domain)
+	}
 	// 面倒なのでtrailing dotは無しで統一
 	if strings.HasSuffix(domain, ".") {
 		return errors.Errorf("trailing dot not allowed in domain %v", domain)
