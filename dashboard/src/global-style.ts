f77c288092ca80e8ff7ae7c494c6dd4d390953b9
detteiu8383 <ysdr83@gmail.com>
2023-11-21 08:41:41 +0900

    :lipstick: スクロールバーのスタイリング

--- dashboard/src/global-style.ts
+++ dashboard/src/global-style.ts
@@ -47,3 +47,29 @@ globalStyle('body', {
 globalStyle('#root', {
   height: '100%',
 })
+
+globalStyle('*', {
+  scrollbarColor: `${colorVars.semantic.ui.tertiary} ${colorVars.semantic.ui.secondary}`,
+  scrollbarWidth: 'thin',
+  transition: 'scrollbar-color 0.3s',
+})
+globalStyle('*:hover, *:active', {
+  scrollbarColor: `${colorVars.semantic.ui.border} ${colorVars.semantic.ui.secondary}`,
+})
+globalStyle('*::-webkit-scrollbar', {
+  width: '6px',
+  height: '6px',
+})
+globalStyle('*::-webkit-scrollbar-corner', {
+  visibility: 'hidden',
+  display: 'none',
+})
+globalStyle('*::-webkit-scrollbar-thumb', {
+  background: colorVars.semantic.ui.tertiary,
+})
+globalStyle('*::-webkit-scrollbar-thumb:hover', {
+  background: colorVars.semantic.ui.border,
+})
+globalStyle('*::-webkit-scrollbar-track', {
+  background: colorVars.semantic.ui.secondary,
+})
