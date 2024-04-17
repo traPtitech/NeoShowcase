b71654c0fe0076b8e384f7abfd049eb5349ab446
motoki317 <motoki317@gmail.com>
2024-04-17 16:22:01 +0900

    Retain search condition up to tab session

--- dashboard/src/libs/localStore.ts
+++ dashboard/src/libs/localStore.ts
@@ -3,12 +3,17 @@ import { createStore } from 'solid-js/store'
 
 // https://www.solidjs.com/examples/todos
 
-export const createLocalSignal = <T>(name: string, init: T): ReturnType<typeof createSignal<T>> => {
-  const localState = localStorage.getItem(name)
+const createStorageSignal = <T>(name: string, init: T, storage: Storage): ReturnType<typeof createSignal<T>> => {
+  const localState = storage.getItem(name)
   const [state, setState] = createSignal<T>(localState ? JSON.parse(localState) : init)
-  createEffect(() => localStorage.setItem(name, JSON.stringify(state())))
+  createEffect(() => storage.setItem(name, JSON.stringify(state())))
   return [state, setState]
 }
+
+export const createSessionSignal = <T>(name: string, init: T): ReturnType<typeof createSignal<T>> =>
+  createStorageSignal(name, init, sessionStorage)
+export const createLocalSignal = <T>(name: string, init: T): ReturnType<typeof createSignal<T>> =>
+  createStorageSignal(name, init, localStorage)
 
 export const createLocalStore = <T extends {}>(name: string, init: T): ReturnType<typeof createStore<T>> => {
   const localState = localStorage.getItem(name)
