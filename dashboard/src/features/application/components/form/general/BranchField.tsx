376e3fadcad8aec88e3b5c826404dfad732f2379
eyemono-moe <ysdr83@gmail.com>
2024-07-27 11:40:08 +0900

    fix: ブランチが見つからなかった時のエラー文面を修正

--- dashboard/src/features/application/components/form/general/BranchField.tsx
+++ dashboard/src/features/application/components/form/general/BranchField.tsx
@@ -14,25 +14,26 @@ const BranchField: Component<Props> = (props) => {
   const { formStore } = useApplicationForm()
   const branches = useBranches(() => props.repo.id)
 
-  // 設定されていたブランチを取得できなかった場合(ブランチが削除された場合等)にエラーを表示し、
+  // 設定されていたブランチが見つからなかった場合(ブランチが削除された場合等)にエラーを表示し、
   // 存在しない refName の代わりに空文字列を入れる
   createEffect(() => {
-    const ref = getValue(
+    const refName = getValue(
       untrack(() => formStore),
       'form.refName',
     )
-    if (ref !== undefined && !branches().includes(ref)) {
+    if (refName !== undefined && !branches().includes(refName)) {
+      // 下の refName に対する setValue により、もともと設定されていたブランチ名が空文字列に上書きされるため
+      // refName が空でない(=refName がもともと設定されていたブランチ名になっている)とき、それを使ってエラーを表示する
+      if (refName !== '') {
+        setError(formStore, 'form.refName', `設定されたブランチ(${refName})は存在しないブランチ名です`)
+      }
+
       // refName を undefined にすると"更新しないプロパティ"という扱いになるため、
       // 空文字列を設定してバリデーションエラーを発生させる
       setValue(
         untrack(() => formStore),
         'form.refName',
         '',
-      )
-      setError(
-        formStore,
-        'form.refName',
-        '設定されていたブランチの取得に失敗しました。リポジトリへのアクセス権を確認してください。',
       )
     } else {
       clearError(formStore, 'form.refName')
@@ -62,6 +63,7 @@ const BranchField: Component<Props> = (props) => {
               label: branch,
               value: branch,
             }))}
+            disabled={branches().length === 0}
             value={field.value}
             error={field.error}
             readOnly={!props.hasPermission}
