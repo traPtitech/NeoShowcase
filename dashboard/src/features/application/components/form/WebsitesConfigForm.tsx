146b0e709fc2d676eb2f184114d5f56f018546c7
eyemono-moe <ysdr83@gmail.com>
2024-08-16 06:21:46 +0900

    wip: websites設定

--- dashboard/src/features/application/components/form/WebsitesConfigForm.tsx
+++ dashboard/src/features/application/components/form/WebsitesConfigForm.tsx
@@ -1,12 +1,18 @@
 import { styled } from '@macaron-css/solid'
-import { createFormStore, getValue, getValues, valiForm } from '@modular-forms/solid'
-import { type Component, For } from 'solid-js'
-import { createMutable, createStore } from 'solid-js/store'
+import { createFormStore, getValues, setValue, valiForm } from '@modular-forms/solid'
+import { type Component, For, createResource, createSignal } from 'solid-js'
+import toast from 'solid-toast'
+import { parse } from 'valibot'
 import type { Application } from '/@/api/neoshowcase/protobuf/gateway_pb'
 import { Button } from '/@/components/UI/Button'
 import { MaterialSymbols } from '/@/components/UI/MaterialSymbols'
-import { systemInfo } from '/@/libs/api'
-import { createWebsiteInitialValues, createWebsiteSchema, websiteMessageToSchema } from '../../schema/websiteSchema'
+import { client, handleAPIError, systemInfo } from '/@/libs/api'
+import {
+  type CreateWebsiteInput,
+  createWebsiteInitialValues,
+  createWebsiteSchema,
+  websiteMessageToSchema,
+} from '../../schema/websiteSchema'
 import WebsiteFieldGroup from './website/WebsiteFieldGroup'
 
 const AddMoreButtonContainer = styled('div', {
@@ -23,17 +29,19 @@ type Props = {
 }
 
 const WebsiteConfigForm: Component<Props> = (props) => {
-  const formStores = createMutable(
-    props.app.websites.map((w) =>
-      createFormStore({
-        initialValues: websiteMessageToSchema(w),
-        validate: async (input) => {
-          console.log(input)
-          console.log(await valiForm(createWebsiteSchema)(input))
-          return valiForm(createWebsiteSchema)(input)
-        },
-      }),
-    ),
+  const [formStores, { mutate }] = createResource(
+    () => props.app.websites,
+    (websites) =>
+      websites.map((w) =>
+        createFormStore<CreateWebsiteInput, undefined>({
+          initialValues: websiteMessageToSchema(w),
+          validate: async (input) => {
+            console.log(input)
+            console.log(await valiForm(createWebsiteSchema)(input))
+            return valiForm(createWebsiteSchema)(input)
+          },
+        }),
+      ),
   )
 
   const availableDomains = systemInfo()?.domains ?? []
@@ -41,17 +49,17 @@ const WebsiteConfigForm: Component<Props> = (props) => {
   const addFormStore = () => {
     const defaultDomain = availableDomains.at(0)
     if (defaultDomain) {
-      formStores.push(
-        createFormStore({
-          initialValues: createWebsiteInitialValues(defaultDomain),
-          validateOn: 'blur',
-          validate: async (input) => {
-            console.log(input)
-            console.log(await valiForm(createWebsiteSchema)(input))
-            return valiForm(createWebsiteSchema)(input)
-          },
-        }),
-      )
+      const newForm = createFormStore<CreateWebsiteInput, undefined>({
+        initialValues: createWebsiteInitialValues(defaultDomain),
+        // validateOn: 'blur',
+        validate: async (input) => {
+          console.log(input)
+          console.log(await valiForm(createWebsiteSchema)(input))
+          return valiForm(createWebsiteSchema)(input)
+        },
+      })
+
+      mutate((websites) => websites?.concat([newForm]))
     }
   }
 
@@ -60,15 +68,70 @@ const WebsiteConfigForm: Component<Props> = (props) => {
     return configCase === 'runtimeBuildpack' || configCase === 'runtimeDockerfile' || configCase === 'runtimeCmd'
   }
 
-  const applyChanges = () => {
-    console.log(formStores.map((f) => getValues(f)))
+  const [isSubmitting, setIsSubmitting] = createSignal(false)
+  const applyChanges = async () => {
+    setIsSubmitting(true)
+
+    try {
+      /**
+       * 送信するWebsite設定
+       * - 変更を保存しないものの、initial value
+       * - 変更して保存するもの ( = `readyToSave`)
+       * - 追加するもの ( = `added`)
+       * - 削除しないもの ( = not `readyToDelete`)
+       */
+      const websitesToSave =
+        formStores()
+          ?.map((form) => {
+            const values = getValues(form)
+            switch (values.state) {
+              case 'noChange':
+                return form.internal.initialValues
+              case 'readyToChange':
+              case 'added':
+                return values
+              case 'readyToDelete':
+                return undefined
+            }
+          })
+          .filter((w): w is Exclude<typeof w, undefined> => w !== undefined) ?? []
+
+      const parsedWebsites = websitesToSave.map((w) => parse(createWebsiteSchema, w))
+
+      await client.updateApplication({
+        id: props.app.id,
+        websites: {
+          websites: parsedWebsites,
+        },
+      })
+      toast.success('ウェブサイト設定を保存しました')
+      void props.refetchApp()
+    } catch (e) {
+      // `readyToChange` を `noChange` に戻す
+      for (const form of formStores() ?? []) {
+        const values = getValues(form)
+        if (values.state === 'readyToChange') {
+          setValue(form, 'state', 'noChange')
+        }
+      }
+      handleAPIError(e, 'Failed to save website settings')
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   return (
     <>
-      <For each={formStores}>
+      <For each={formStores()}>{(formStore) => <pre>{JSON.stringify(getValues(formStore), null, 2)}</pre>}</For>
+      <For each={formStores()}>
         {(formStore) => (
-          <WebsiteFieldGroup formStore={formStore} isRuntimeApp={isRuntimeApp()} applyChanges={applyChanges} />
+          <WebsiteFieldGroup
+            formStore={formStore}
+            isRuntimeApp={isRuntimeApp()}
+            applyChanges={applyChanges}
+            isSubmitting={isSubmitting()}
+            readonly={!props.hasPermission}
+          />
         )}
       </For>
       <AddMoreButtonContainer>
--- dashboard/src/pages/apps/[id]/settings/urls.tsx
+++ dashboard/src/pages/apps/[id]/settings/urls.tsx
@@ -1,116 +1,15 @@
-import { createFormStore, getValue, getValues, setValue } from '@modular-forms/solid'
-import { Show, createResource } from 'solid-js'
-import toast from 'solid-toast'
-import { DeployType } from '/@/api/neoshowcase/protobuf/gateway_pb'
+import { Show } from 'solid-js'
 import { DataTable } from '/@/components/layouts/DataTable'
-import { type WebsiteFormStatus, WebsiteSettings, newWebsite } from '/@/components/templates/app/WebsiteSettings'
 import { ApplicationFormProvider } from '/@/features/application/provider/applicationFormProvider'
-import { client, handleAPIError } from '/@/libs/api'
 import { useApplicationData } from '/@/routes'
 import WebsiteConfigForm from '../../../../features/application/components/form/WebsitesConfigForm'
 
 export default () => {
   const { app, refetch, hasPermission } = useApplicationData()
 
-  const [websiteForms, { mutate }] = createResource(
-    () => app()?.websites,
-    (websites) => {
-      return websites.map((website) => {
-        const form = createFormStore<WebsiteFormStatus>({
-          initialValues: {
-            state: 'noChange',
-            website: structuredClone(website),
-          },
-        })
-        return form
-      })
-    },
-  )
-  const addWebsiteForm = () => {
-    const form = createFormStore<WebsiteFormStatus>({
-      initialValues: {
-        state: 'added',
-        website: newWebsite(),
-      },
-    })
-    mutate((forms) => {
-      return forms?.concat([form])
-    })
-  }
-  const deleteWebsiteForm = (index: number) => {
-    if (!websiteForms.latest) return
-
-    const state = getValue(websiteForms()[index], 'state')
-    if (state === 'added') {
-      mutate((forms) => {
-        return forms?.filter((_, i) => i !== index)
-      })
-    } else {
-      setValue(websiteForms()[index], 'state', 'readyToDelete')
-      handleApplyChanges()
-    }
-  }
-
-  const handleApplyChanges = async () => {
-    try {
-      /**
-       * 送信するWebsite設定
-       * - 変更を保存しないものの、initial value
-       * - 変更して保存するもの ( = `readyToSave`)
-       * - 追加するもの ( = `added`)
-       * - 削除しないもの ( = not `readyToDelete`)
-       */
-      const websitesToSave = websiteForms()
-        ?.map((form) => {
-          const values = getValues(form)
-          switch (values.state) {
-            case 'noChange':
-              return form.internal.initialValues.website
-            case 'readyToChange':
-              return values.website
-            case 'added':
-              return values.website
-            case 'readyToDelete':
-              return undefined
-          }
-        })
-        .filter((w): w is Exclude<typeof w, undefined> => w !== undefined)
-
-      await client.updateApplication({
-        id: app()?.id,
-        websites: {
-          websites: websitesToSave,
-        },
-      })
-      toast.success('ウェブサイト設定を保存しました')
-      void refetch()
-    } catch (e) {
-      // `readyToChange` を `noChange` に戻す
-      for (const form of websiteForms() ?? []) {
-        const values = getValues(form)
-        if (values.state === 'readyToChange') {
-          setValue(form, 'state', 'noChange')
-        }
-      }
-      handleAPIError(e, 'Failed to save website settings')
-    }
-  }
-
   return (
     <DataTable.Container>
       <DataTable.Title>URLs</DataTable.Title>
-      <Show when={websiteForms()}>
-        {(nonNullForms) => (
-          <WebsiteSettings
-            isRuntimeApp={app()?.deployType === DeployType.RUNTIME}
-            formStores={nonNullForms()}
-            addWebsite={addWebsiteForm}
-            deleteWebsiteForm={deleteWebsiteForm}
-            applyChanges={handleApplyChanges}
-            hasPermission={hasPermission()}
-          />
-        )}
-      </Show>
       <Show when={app()}>
         <ApplicationFormProvider>
           <WebsiteConfigForm app={app()!} hasPermission={hasPermission()} refetchApp={refetch} />
