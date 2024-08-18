5f85b92ec5627f5c7a3d98428578d456cbc91127
cp-20 <n.naoki.1587@gmail.com>
2024-08-18 10:36:53 +0900

    `uno.config.ts` を設定

---
+++ dashboard/uno.config.ts
@@ -0,0 +1,14 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetIcons(),
+  ],
+  transformers: [transformerVariantGroup()],
+});
