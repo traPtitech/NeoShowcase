d13c8a52484d6cf44a814a26a83f1c1edd56116e
cp-20 <n.naoki.1587@gmail.com>
2024-08-24 17:09:24 +0900

    `virtual:uno.css` をimportした

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -3,5 +3,6 @@ import { render } from 'solid-js/web'
 
 import App from './App'
 import './global-style'
+import 'virtual:uno.css'
 
 render(() => <App />, document.getElementById('root') as HTMLElement)
