92d895a282225645c02d71fdf358a27ea6021595
cp-20 <n.naoki.1587@gmail.com>
2024-10-29 14:39:26 +0900

    animation 直した

--- dashboard/src/components/templates/app/AppsFilter.tsx
+++ dashboard/src/components/templates/app/AppsFilter.tsx
@@ -53,7 +53,10 @@ const AppsFilter: Component<{
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content
-          class="-translate-y-2 z-1 grid max-w-[--kb-popper-content-available-width] origin-[--kb-menu-content-transform-origin] grid-cols-[repeat(3,auto)] grid-rows-[1fr_auto] gap-2 overflow-x-auto rounded-md bg-ui-primary p-4 opacity-0 shadow-default data-[expanded]:translate-y-0 data-[expanded]:opacity-1"
+          class={clsx(
+            'z-1 grid max-w-[--kb-popper-content-available-width] origin-[--kb-menu-content-transform-origin] grid-cols-[repeat(3,auto)] grid-rows-[1fr_auto] gap-2 overflow-x-auto rounded-md bg-ui-primary p-4 shadow-default',
+            'animate-duration-200 animate-ease-in-out animate-wipe-hide-up data-[expanded]:animate-wipe-show-down',
+          )}
           style={{
             'grid-template-areas': `
               "status provider sort"
